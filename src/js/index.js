d941de053a11770a0395184867df557695d3fa4c
AlexVSi <aleksandrsidorenko@MacBook-Air-Aleksandr.local>
2023-10-01 15:08:43 +0300

    Слайдер!!!

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// Отступы =======================================================
 resizeWindow()
 
 window.addEventListener('resize', (e) => {
@@ -14,3 +15,64 @@ function resizeWindow() {
 	const smileArea = document.querySelector('.smile__area')
 	smileArea.style.paddingRight = marg
 }
+
+// Слайдер =======================================================
+class Web {
+	constructor(title, text, link, imgPath) {
+		this.title = title
+		this.text = text
+		this.link = link
+		this.imgPath = imgPath
+	}
+}
+
+let webs = [
+	salniki = new Web('Сайт <br/> для автосервиса', 'с онлайн-калькулятором и SEO-оптимизацией', 'https://www.reica.md', './img/salniki.jpg'),
+	glavLendStroy = new Web('Сайт <br/> для строительной компании', 'крупная строительная компания', 'https://glavlandstroy.ru', './img/GlavLendStroy.jpg'),
+	asphalt = new Web('Сайт <br/> для дорожной компании', 'с онлайн калькулятором', 'https://asp-glavlandstroy.ru', './img/asphalt.jpg'),
+	businessWeek = new Web('Сайт <br/> посвященный бизнес-событию', 'с онлайн калькулятором', 'https://businessweek.by', './img/businessWeek.jpg'),
+]
+
+console.log(webs)
+
+// Получаем элементы слайдера
+const slider = document.querySelector('.portfolio__slider');
+const prevButton = document.querySelector('.back__button');
+const nextButton = document.querySelector('.next__button');
+// const slides = Array.from(slider.querySelectorAll('.portfolio__slider'));
+const slideCount = webs.length
+let slideIndex = 0;
+
+const img = document.querySelector('.portfolio__img')
+const title = document.querySelector('.work__title')
+const text = document.querySelector('.work__text')
+const link = document.querySelector('.see__button')
+
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+	let web = webs[slideIndex]
+	img.setAttribute('src', `${web.imgPath}`)
+	title.innerHTML = web.title
+	text.textContent = web.text
+	link.href = web.link
+	console.log(web.title)
+}
+
+// Инициализация слайдера
+updateSlider();
--- src/js/modules/slider.js
+++
@@ -1,8 +0,0 @@
-class web {
-	constructor(name, text, link) {
-		this.name = name
-		this.text = text
-		this.link - link
-	}
-}
-
